dfbff2c291d1d7fcc0a9944928d25c0eebfd3df8
lutfor <lutfor@lu>
2014-05-31 17:27:26 -0230

    initial checkin

---
+++ Firstproject/src/day1/MyFirstClass.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class MyFirstClass {
+	/*
+	 * comment in here
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("hello ki khobor");
+	}
+
+}
---
+++ Firstproject/src/day1/example/MyFirstClass.java
@@ -0,0 +1,10 @@
+ package day1.example;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("day one example how to use git");
+	}
+
+}
