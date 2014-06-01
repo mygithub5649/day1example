fd01b61dc454454bbd9fe007b0d844dc717a109d
mygithub5649 <maskbd@gmail.com>
2014-05-31 23:54:35 -0230

    Update MyFirstClass.java

--- Firstproject/src/day1/MyFirstClass.java
+++ Firstproject/src/day1/MyFirstClass.java
@@ -9,6 +9,8 @@ public class MyFirstClass {
 		// TODO Auto-generated method stub
     System.out.println("hello ki khobor");
     System.out.println("git example");
+     System.out.println("git example let's see");
+    
 	}
 
 }
